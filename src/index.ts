c69a1bc53a47d4b2a5e1f2de0697ec11f4af4e06
Dmitry Zorin <forestbeast1@gmail.com>
2021-12-17 18:53:19 +0300

    feature: added creating polyline, creating markers and calculate distance

--- src/index.ts
+++ src/index.ts
@@ -20,30 +20,140 @@ import "./style.css";
 // the first trans-Pacific flight between Oakland, CA, and Brisbane,
 // Australia which was made by Charles Kingsford Smith.
 
+let markers: google.maps.Marker[] = [];
+let polyLines: google.maps.Polyline[] = [];
+
+
 function initMap(): void {
-  const map = new google.maps.Map(
-    document.getElementById("map") as HTMLElement,
-    {
-      zoom: 3,
-      center: { lat: 0, lng: -180 },
-      mapTypeId: "terrain",
-    }
-  );
+    const map = new google.maps.Map(
+        document.getElementById("map") as HTMLElement,
+        {
+            zoom: 12,
+            center: {lat: 42.356455529300256, lng: -71.1167065923323},
+            mapTypeId: "terrain",
+            disableDefaultUI: true
+        }
+    );
+    const infowindow = new google.maps.InfoWindow();
+    (document.getElementById("add") as HTMLElement).addEventListener(
+        "click",
+        () => addMarker(map, infowindow)
+    );
 
-  const flightPlanCoordinates = [
-    { lat: 37.772, lng: -122.214 },
-    { lat: 21.291, lng: -157.821 },
-    { lat: -18.142, lng: 178.431 },
-    { lat: -27.467, lng: 153.027 },
-  ];
-  const flightPath = new google.maps.Polyline({
-    path: flightPlanCoordinates,
-    geodesic: true,
-    strokeColor: "#FF0000",
-    strokeOpacity: 1.0,
-    strokeWeight: 2,
-  });
+    (document.getElementById("draw") as HTMLElement).addEventListener(
+        "click",
+        () => drawPolyLine(map)
+    );
 
-  flightPath.setMap(map);
+    (document.getElementById("delete-all-markers") as HTMLElement).addEventListener(
+        "click",
+        () => deleteMarkers()
+    );
+    (document.getElementById("delete-all-lines") as HTMLElement).addEventListener(
+        "click",
+        () => deleteLines()
+    );
+    (document.getElementById("p1") as HTMLElement).addEventListener(
+        "change",
+        () => updateDistance()
+    );
+    (document.getElementById("p2") as HTMLElement).addEventListener(
+        "change",
+        () => updateDistance()
+    );
+
 }
-export { initMap };
+
+function drawPolyLine(map: google.maps.Map) {
+    const input = (document.getElementById("coordinates") as HTMLInputElement).value;
+    const polylinePath = input.split("\n").filter(s => s !== "").map(geoStr => {
+            const geo = geoStr.split(",");
+            return {
+                lat: Number(geo[0]),
+                lng: Number(geo[1])
+            }
+        }
+    );
+    const polyLine = new google.maps.Polyline({
+        path: polylinePath,
+        geodesic: true,
+        strokeColor: "#FF0000",
+        strokeOpacity: 1.0,
+        strokeWeight: 2,
+        map: map
+    });
+    polyLines.push(polyLine);
+    map.setCenter(polylinePath[Math.round(polylinePath.length / 2 - 1)]);
+    map.setZoom(15);
+}
+
+function addMarker(
+    map: google.maps.Map,
+    infowindow: google.maps.InfoWindow
+) {
+    const positionInput = (document.getElementById("latlng") as HTMLInputElement).value;
+    const markerName = (document.getElementById("marker-name") as HTMLInputElement).value;
+    const latlngStr = positionInput.split(",", 2);
+    const position = {
+        lat: parseFloat(latlngStr[0]),
+        lng: parseFloat(latlngStr[1]),
+    };
+    infowindow.setContent(markerName)
+
+    const marker = new google.maps.Marker({
+        position: position,
+        map: map
+    });
+    google.maps.event.addListener(marker, "click", () => {
+        infowindow.open(map, marker);
+    });
+    markers.push(marker);
+    map.setCenter(position);
+    map.setZoom(15);
+}
+
+function deleteMarkers() {
+    markers.forEach(marker => marker.setMap(null));
+    markers = [];
+}
+
+function deleteLines() {
+    polyLines.forEach(line => line.setMap(null));
+    polyLines = [];
+}
+
+function updateDistance() {
+    const pStr1 = (document.getElementById("p1") as HTMLInputElement).value;
+    const pStr2 = (document.getElementById("p2") as HTMLInputElement).value;
+    const p1 = pStr1.split(",")
+    const p2 = pStr2.split(",")
+    const geoPoint1 = {
+        lat: parseFloat(p1[0]),
+        lon: parseFloat(p1[1]),
+    };
+    const geoPoint2 = {
+        lat: parseFloat(p2[0]),
+        lon: parseFloat(p2[1]),
+    };
+    let distance = haversineDistanc(geoPoint1, geoPoint2);
+    if (isNaN(distance)) {
+        distance = 0
+    }
+    (document.getElementById("distance") as HTMLInputElement).innerHTML = `${distance.toFixed(2)}m`;
+}
+
+function haversineDistanc(p1, p2) {
+    const earthRadius = 6371e3;
+    const radLat1 = p1.lat * Math.PI / 180;
+    const radLat2 = p2.lat * Math.PI / 180;
+    const lat = (p2.lat - p1.lat) * Math.PI / 180;
+    const lon = (p2.lon - p1.lon) * Math.PI / 180;
+
+    const a = Math.sin(lat / 2) * Math.sin(lat / 2) +
+        Math.cos(radLat1) * Math.cos(radLat2) *
+        Math.sin(lon / 2) * Math.sin(lon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return earthRadius * c;
+}
+
+export {initMap};
